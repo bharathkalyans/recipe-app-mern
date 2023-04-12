ae877896b9272691f5fa8c1baa6661402b0b6867
Bharath Kalyan S <sbharath35@gmail.com>
2023-04-12 11:31:01 +0530

    Completed the User Backend part

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+
+const userRouter = require("./routes/users.js");
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+
+mongoose.connect(
+  `mongodb+srv://bharathkalyans:${process.env.MONGODB_PWD}@recipes.orv6t0h.mongodb.net/recipes?retryWrites=true&w=majority`
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is started at PORT : ${PORT}`);
+});
---
+++ server/src/routes/users.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const UserModel = require("../models/Users");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await UserModel.findOne({ username: username });
+    if (user) {
+      res.send({ message: "Username already exists!! 🙂!" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.send({ message: "User registered Successfully!!" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username: username });
+    if (!user) {
+      res.send({ message: "User doesn't Exist!" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.send({ message: "Invalid Username or Password!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.send({ token, userID: user._id });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/", (req, res) => {
+  res.send("Auth Sucessfull");
+});
+
+module.exports = router;

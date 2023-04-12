d23b3c2e26e5554c7f0e6a04dad55552533286a0
Bharath Kalyan S <sbharath35@gmail.com>
2023-04-12 11:56:17 +0530

    Created Routes for the FrontEnd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import CreateRecipe from "./pages/CreateRecipe";
+import SavedRecipes from "./pages/SaveRecipes";
 
 function App() {
+  // http://localhost:3003/auth/login
 
-
-    return (
-        <div className="App">
-            <h1>Helloo</h1>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;

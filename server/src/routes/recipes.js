3be2522ffecdfb67797fdbe3be076c3d469646d1
Bharath Kalyan S <sbharath35@gmail.com>
2023-04-14 12:18:30 +0530

    Changed the get endpoint to receive userID from Params

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -36,9 +36,9 @@ router.put("/", async (req, res) => {
   }
 });
 
-router.get("/savedRecipes/ids", async (req, res) => {
+router.get("/savedRecipes/ids/:userID", async (req, res) => {
   try {
-    const user = await UserModel.findById(req.body.userID);
+    const user = await UserModel.findById(req.params.userID);
     res.send({ savedRecipes: user?.savedRecipes });
   } catch (error) {
     res.json(error);
